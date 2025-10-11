1c9b087ec7c9afc9f086ad74f617dc6122e5d69c
drawroFBackward <amandidwania@gmail.com>
2025-10-11 09:45:13 +0900

    removed extra linebreak

--- main.cpp
+++ main.cpp
@@ -129,7 +129,7 @@ int IntType::divide(int lhs, int rhs)
 {
     if (rhs == 0)
     {
-        std::cout << std::endl << "error, integer division by zero will crash the program!" << std::endl;
+        std::cout << "error, integer division by zero will crash the program!" << std::endl;
         std::cout << "returning lhs" << std::endl;
         return lhs;
     }
